153e51e7574e40377d2a08d7e9cf232bff2f9d9a
Gabriela <gabrielakborborema@gmail.com>
2024-09-04 15:15:36 -0300

    initial commit

---
+++ app/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Gabriela')`
+
+app.get('/',  (req, res) => {
+
+    connection.query(sql, () => {
+        connection.query("SELECT name FROM people", (err, result, fields) => {
+            if (err) throw err;
+            
+            res.send(`<h1>Full Cycle Rocks!<h1>${result.map(element => {
+                return `<li>${element.name}</li>`}).join('')}`
+            )
+        });
+    })
+
+})
+
+app.listen(port, () => {
+    console.log('Rodando na porta ' + port)
+})
