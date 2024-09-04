20c44c2a798322f341a70bd5583e941e8395c101
Gabriela <gabrielakborborema@gmail.com>
2024-09-04 15:28:30 -0300

    now table people is created in db if not exists

--- app/index.js
+++ app/index.js
@@ -15,17 +15,17 @@ const connection = mysql.createConnection(config)
 const sql = `INSERT INTO people(name) values('Gabriela')`
 
 app.get('/',  (req, res) => {
-
-    connection.query(sql, () => {
-        connection.query("SELECT name FROM people", (err, result, fields) => {
-            if (err) throw err;
-            
-            res.send(`<h1>Full Cycle Rocks!<h1>${result.map(element => {
-                return `<li>${element.name}</li>`}).join('')}`
-            )
-        });
+    connection.query("CREATE TABLE IF NOT EXISTS people (id int not null auto_increment, name varchar(255), primary key(id))", () => {
+        connection.query(sql, () => {
+            connection.query("SELECT name FROM people", (err, result, fields) => {
+                if (err) throw err;
+                
+                res.send(`<h1>Full Cycle Rocks!<h1>${result.map(element => {
+                    return `<li>${element.name}</li>`}).join('')}`
+                )
+            });
+        })
     })
-
 })
 
 app.listen(port, () => {
